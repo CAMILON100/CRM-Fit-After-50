13814096fa643d17110eca78a84684a4d79d67d6
Jos <jos@MBP-de-Jos.lan>
2020-09-22 17:05:11 -0300

    Master

--- cache/modules/Alerts/language/en_us.lang.php
+++ cache/modules/Alerts/language/en_us.lang.php
@@ -1,5 +1,5 @@
 <?php
-// created: 2020-09-22 18:03:13
+// created: 2020-09-22 18:16:49
 $mod_strings = array (
   'LBL_ID' => 'ID',
   'LBL_DATE_ENTERED' => 'Date Created',
--- cache/modules/Campaigns/language/en_us.lang.php
+++ cache/modules/Campaigns/language/en_us.lang.php
@@ -1,5 +1,5 @@
 <?php
-// created: 2020-09-22 18:03:03
+// created: 2020-09-22 18:17:04
 $mod_strings = array (
   'LBL_ID' => 'ID',
   'LBL_DATE_ENTERED' => 'Date Created',
